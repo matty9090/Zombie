0d59dec039fa44e9a99c39359a6fdae2b0a45af4
Matthew Lowe <lowe.matthew@mail.com>
2019-11-12 16:43:21 +0000

    Show a tile where the user is hovering

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -10,6 +10,7 @@ public class Game : MonoBehaviour
     [SerializeField] private Canvas Menu = null;
     [SerializeField] private Canvas Hud = null;
     [SerializeField] private Transform CharacterStart = null;
+    [SerializeField] private GameObject HoverTile = null;
 
     private RaycastHit[] mRaycastHits;
     private Character mCharacter;
@@ -23,20 +24,28 @@ public class Game : MonoBehaviour
         mMap = GetComponentInChildren<Environment>();
         mCharacter = Instantiate(Character, transform); 
         ShowMenu(true);
+        HoverTile.GetComponent<MeshRenderer>().enabled = false;
     }
 
     private void Update()
     {
+        HoverTile.GetComponent<MeshRenderer>().enabled = false;
+
         // Check to see if the player has clicked a tile and if they have, try to find a path to that 
         // tile. If we find a path then the character will move along it to the clicked tile. 
-        if(Input.GetMouseButtonDown(0))
+        Ray screenClick = MainCamera.ScreenPointToRay(Input.mousePosition);
+        int hits = Physics.RaycastNonAlloc(screenClick, mRaycastHits);
+        
+        if (hits > 0)
         {
-            Ray screenClick = MainCamera.ScreenPointToRay(Input.mousePosition);
-            int hits = Physics.RaycastNonAlloc(screenClick, mRaycastHits);
-            if( hits > 0)
+            EnvironmentTile tile = mRaycastHits[0].transform.GetComponent<EnvironmentTile>();
+            
+            if (tile != null)
             {
-                EnvironmentTile tile = mRaycastHits[0].transform.GetComponent<EnvironmentTile>();
-                if (tile != null)
+                HoverTile.transform.position = tile.Position;
+                HoverTile.GetComponent<MeshRenderer>().enabled = true;
+
+                if (Input.GetMouseButtonDown(0))
                 {
                     List<EnvironmentTile> route = mMap.Solve(mCharacter.CurrentPosition, tile);
                     mCharacter.GoTo(route);
