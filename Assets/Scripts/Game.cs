a41acbd3e10e67b990337e8dc7e2e0922c8d749e
Matthew Lowe <lowe.matthew@mail.com>
2019-11-15 19:40:35 +0000

    Convert menu to use state system

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     [SerializeField] private Texture2D EnemySkin = null;
-    [SerializeField] private Canvas Menu = null;
-    [SerializeField] private Canvas Hud = null;
-    [SerializeField] private Transform CharacterStart = null;
     [SerializeField] private HealthBar HealthBar = null;
     [SerializeField] private int BuildingTime = 90;
 
+    [SerializeField] public Canvas Menu = null;
+    [SerializeField] public Canvas Hud = null;
     [SerializeField] public Camera MainCamera = null;
     [SerializeField] public Character Character = null;
     [SerializeField] public GameObject HoverTile = null;
+    [SerializeField] public Transform CharacterStart = null;
     
     public Character CharacterInst { get; private set;  }
     public Resources Resources { get; set; }
     public Environment Map { get; private set; }
 
-    private enum EGameState { Building, Wave, GameOver };
+    private enum EGameState { Menu, Building, Wave, GameOver };
     private RaycastHit[] mRaycastHits;
     private EGameState mGameState = EGameState.Building;
     private float BuildingTimer = 0.0f;
@@ -35,15 +35,15 @@ public class Game : MonoBehaviour
         CharacterInst = Instantiate(Character, transform);
         BuildingTimer = (float)BuildingTime;
         HealthBar.ProvideCharacter(CharacterInst);
-        ShowMenu(true);
 
         mStates = new Dictionary<EGameState, IState>
         {
+            [EGameState.Menu]     = new StateMenu(),
             [EGameState.Building] = new StateBuilding(),
             [EGameState.Wave]     = new StateWave()
         };
 
-        SwitchState(EGameState.Building);
+        SwitchState(EGameState.Menu);
     }
 
     private void SwitchState(EGameState state)
@@ -79,24 +79,7 @@ public class Game : MonoBehaviour
 
     public void ShowMenu(bool show)
     {
-        if (Menu != null && Hud != null)
-        {
-            Menu.enabled = show;
-            Hud.enabled = !show;
-
-            if (show)
-            {
-                CharacterInst.transform.position = CharacterStart.position;
-                CharacterInst.transform.rotation = CharacterStart.rotation;
-                Map.CleanUpWorld();
-            }
-            else
-            {
-                CharacterInst.transform.position = Map.Start.Position;
-                CharacterInst.transform.rotation = Quaternion.identity;
-                CharacterInst.CurrentPosition = Map.Start;
-            }
-        }
+        SwitchState(show ? EGameState.Menu : EGameState.Building);
     }
 
     public void UIBuildingClicked(UIBuilding element)
---
+++ Assets/Scripts/StateMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMenu : IState
+{
+    private Game Game = null;
+
+    public StateMenu()
+    {
+        Game = GameObject.Find("Game").GetComponent<Game>();
+    }
+
+    public void OnEnter()
+    {
+        ShowMenu(true);
+    }
+
+    public void OnExit()
+    {
+        ShowMenu(false);
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    private void ShowMenu(bool show)
+    {
+        if (Game.Menu != null && Game.Hud != null)
+        {
+            Game.Menu.enabled = show;
+            Game.Hud.enabled = !show;
+
+            if (show)
+            {
+                Game.CharacterInst.transform.position = Game.CharacterStart.position;
+                Game.CharacterInst.transform.rotation = Game.CharacterStart.rotation;
+                Game.Map.CleanUpWorld();
+            }
+            else
+            {
+                Game.CharacterInst.transform.position = Game.Map.Start.Position;
+                Game.CharacterInst.transform.rotation = Quaternion.identity;
+                Game.CharacterInst.CurrentPosition = Game.Map.Start;
+            }
+        }
+    }
+}
