47de761153717dee92235775878bd78a9574053e
matty9090 <lowe.matthew@mail.com>
2019-12-17 17:51:56 +0000

    Fix being able to move player around on main menu

---
+++ Assets/Scripts/Camera/DefaultCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultCamera : MonoBehaviour, ICamera
+{
+    public void SetEnabled(bool e)
+    {
+        
+    }
+
+	public void SetCharacter(Character c)
+    {
+
+    }
+}
