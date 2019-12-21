7426a41d72b9c90b5bac1ebb62f543e1a65e53f5
matty9090 <lowe.matthew@mail.com>
2019-12-21 21:25:02 +0000

    Disable face light on build state

--- Assets/Scripts/States/StateBuilding.cs
+++ Assets/Scripts/States/StateBuilding.cs
@@ -33,9 +33,11 @@ public class StateBuilding : IState
         GameObject.Find("Directional Light").GetComponent<Light>().color = Game.DayColour;
         RenderSettings.ambientLight = Game.DayColour / 1.2f;
 
-        var charLight = Game.CharacterInst.GetComponentInChildren<Light>();
-        charLight.enabled = false;
-        charLight.intensity = 8.0f;
+        var charLights = Game.CharacterInst.GetComponentsInChildren<Light>();
+        charLights[0].enabled = false;
+        charLights[0].intensity = 8.0f;
+        charLights[1].enabled = false;
+        charLights[1].intensity = 0.34f;
 
         mControllerState = EControllerState.Idle;
         Game.CharacterInst.ResetHealth();
