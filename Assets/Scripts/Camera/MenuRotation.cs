f6697f04450a7d6b7041ba2ebb2524065c82b192
Matthew Lowe <lowe.matthew@mail.com>
2020-01-21 23:13:43 +0000

    Fix menu rotation and edit tutorial

--- Assets/Scripts/Camera/MenuRotation.cs
+++ Assets/Scripts/Camera/MenuRotation.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class MenuRotation : MonoBehaviour
 {
-    [SerializeField] private float RotationSpeed = 0.2f;
+    [SerializeField] private float RotationSpeed = 10.0f;
 
     void Update()
     {
-        transform.Rotate(new Vector3(0.0f, RotationSpeed, 0.0f));
+        transform.Rotate(new Vector3(0.0f, RotationSpeed * Time.deltaTime, 0.0f));
     }
 }
